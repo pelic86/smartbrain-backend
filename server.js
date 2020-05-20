6e3d815664d4549d9b3fec8a7046c2b4666dca0c
pelic86 <pelicwei@gmail.com>
2020-05-20 23:53:09 +0800

    adding port

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ app.get('/profile/:id', profile.handleProfileGet(db))
 app.put('/image', image.handleImage(db))
 app.post('/imageurl', image.handleApiCall())
 
-app.listen(3000, () => {
-    console.log('app is running')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('app is running on port ${process.env.PORT}')
 })
 
 
